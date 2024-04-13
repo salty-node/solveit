3b91f2760e410f9918d652b6a3f88a5fc5ed1d24
CihanAWS-at-165874772638 <cihanilhan.171@gmail.com>
2024-04-13 23:17:21 +0300

    stripe conf test 23

--- StripeService.ts
+++ StripeService.ts
@@ -1,71 +1,79 @@
 import {
-  GenezioDeploy,
-  GenezioHttpRequest,
-  GenezioHttpResponse,
-  GenezioMethod,
+    GenezioDeploy,
+    GenezioHttpRequest,
+    GenezioHttpResponse,
+    GenezioMethod,
 } from "@genezio/types";
 import Stripe from "stripe";
 import pg from "pg";
 
-const { Pool } = pg;
+const {Pool} = pg;
 // Use the Stripe API Key clientSecret to initialize the Stripe Object
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 @GenezioDeploy()
 export class StripeService {
-  pool = new Pool({
-    connectionString: process.env["SOLVEIT_DATABASE_URL"],
-    ssl: true,
-  });
-
-  async createCheckoutSession(): Promise<string> {
-    const stripePromise = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price_data: {
-            currency: "RON",
-            product_data: {
-              name: "Tokens",
-            },
-            unit_amount: 1000, // 20.00 USD
-          },
-          quantity: 1,
-        },
-      ],
-      mode: "payment",
-      success_url: `${process.env.FRONTEND_URL}?success=true`,
-      cancel_url: `${process.env.FRONTEND_URL}?canceled=true`,
+    pool = new Pool({
+        connectionString: process.env["SOLVEIT_DATABASE_URL"],
+        ssl: true,
     });
 
-    return stripePromise.url || "";
-  }
+    async createCheckoutSession(userId: string): Promise<string> {
+        const stripePromise = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price_data: {
+                        currency: "RON",
+                        product_data: {
+                            name: "Tokens",
+                        },
+                        unit_amount: 1000, // 20.00 USD
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: "payment",
+            success_url: `${process.env.FRONTEND_URL}?success=true`,
+            cancel_url: `${process.env.FRONTEND_URL}?canceled=true`,
+            metadata: {
+                userId: userId,
+            },
 
-  @GenezioMethod({ type: "http" })
-  async webhook(req: GenezioHttpRequest): Promise<GenezioHttpResponse> {
-    let event: Stripe.Event;
+        });
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.rawBody,
-        req.headers["stripe-signature"],
-        process.env.STRIPE_WEBHOOK_SECRET!
-      );
-    } catch (err) {
-      return { statusCode: "400", body: "Webhook Error" };
+        return stripePromise.url || "";
     }
 
-    // Handle the checkout.session.completed event
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object;
-      console.log("Fulfilling order", session);
+    @GenezioMethod({type: "http"})
+    async webhook(req: GenezioHttpRequest): Promise<GenezioHttpResponse> {
+        let event: Stripe.Event;
 
-      // TODO: your own custom fulfillment process
-      await this.pool.query(
-        "update credits set credits = credits + 10 where userId = $1",
-        ["efa7b4df-59bf-4da8-9979-0afddd165f53"]
-      );
+        try {
+            event = stripe.webhooks.constructEvent(
+                req.rawBody,
+                req.headers["stripe-signature"],
+                process.env.STRIPE_WEBHOOK_SECRET!
+            );
+        } catch (err) {
+            return {statusCode: "400", body: "Webhook Error"};
+        }
+
+        // Handle the checkout.session.completed event
+        if (event.type === "checkout.session.completed") {
+            const session = event.data.object;
+            console.log("Fulfilling order", session);
+
+            // TODO: your own custom fulfillment process
+            // @ts-ignore
+            const userId = session.metadata.userId;
+            // @ts-ignore
+            const backendService = new BackendService();
+            await backendService.increaseCredits(userId);
+
+            console.log("Order fulfilled successfully"); // Log when the order is fulfilled successfully
+            return {statusCode: "200", body: "Order fulfilled"};
+        }
+
+        return {statusCode: "200", body: "Success"};
     }
-
-    return { statusCode: "200", body: "Success" };
-  }
 }
